1edf7ea86498cd2a582acc175b742a7c48e00c46
Srini Nagaraju <37597474+srinihashi@users.noreply.github.com>
2020-12-03 12:02:42 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+region     = "us-east-1"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-db24d8b6"
+instance_type   = "t2.micro"
+#subnet_id              = "subnet-c02e6198"
+#vpc_security_group_ids = ["sg-b1fe76ca"]
+   tags = {
+       Owner = "Srini"
+       Created-via = "TFC-tfcb-agent"
+    }
+}
