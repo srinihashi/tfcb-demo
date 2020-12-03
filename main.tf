3f7703bed7e775835053219bc48067cdc2eb0986
Srini Nagaraju <37597474+srinihashi@users.noreply.github.com>
2020-12-03 12:31:56 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,12 @@ region     = "us-east-1"
 }
 resource "aws_instance" "web" {
   ami           = "ami-db24d8b6"
-instance_type   = "t2.micro"
-subnet_id              = "subnet-0844f09ccfb753e74"
-#vpc_security_group_ids = ["sg-b1fe76ca"]
-   tags = {
-       Owner = "Srini"
-       Created-via = "TFC:tfcb-agent"
-    }
+  instance_type   = "t2.micro"
+  key_name       = var.key_name
+  subnet_id              = "subnet-0844f09ccfb753e74"
+  #vpc_security_group_ids = ["sg-b1fe76ca"]
+  tags = {
+      Owner = "Srini"
+      Created-via = "TFC:tfcb-agent"
+   }
 }
